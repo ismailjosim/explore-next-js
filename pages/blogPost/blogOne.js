62c87b02a1e0d45a0df255c45c0a85a20d9e1292
JC DESIGN <ismailjosim99@gmail.com>
2022-12-08 00:09:41 +0600

    learn route and nexted route

---
+++ pages/blogPost/blogOne.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const blogOne = () => {
+    return (
+        <div>
+            <h3>How to create a nested route</h3>
+            <p>
+                Create a new folder and into that folder create a file and add some text. then from browser type localhost/foldername/filename
+            </p>
+        </div>
+
+    )
+}
+
+export default blogOne
